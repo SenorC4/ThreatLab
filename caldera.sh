18eb676390b07bfa541c3ef61204c491645f4e5b
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2024-12-20 17:30:40 -0500

    Update caldera.sh

--- caldera.sh
+++ caldera.sh
@@ -2,10 +2,10 @@
 #Used to install mitre caldera 5.0.0 on ubuntu 24.04.1 
 
 sudo apt update && apt upgrade -y
-sudo apt install git python3 python3-pip npmsudo golang 
+sudo apt install git python3 python3-pip npm golang 
 git clone https://github.com/mitre/caldera.git --recursive
 cd caldera
 python3 -m venv .
-sudo ./bin/pip3 install -r requirements.txt
 sudo ./bin/pip3 install docker setuputils
+sudo ./bin/pip3 install -r requirements.txt
 ./bin/python3 server.py --build
