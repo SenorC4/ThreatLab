c7459db6d9128b48639a1b694db3ce652dcdbcce
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2024-12-20 17:31:57 -0500

    Update caldera.sh

--- caldera.sh
+++ caldera.sh
@@ -6,6 +6,6 @@ sudo apt install git python3 python3-pip npm golang
 git clone https://github.com/mitre/caldera.git --recursive
 cd caldera
 python3 -m venv .
-sudo ./bin/pip3 install docker setuputils
+sudo ./bin/pip3 install -r docker setuputils
 sudo ./bin/pip3 install -r requirements.txt
 ./bin/python3 server.py --build
